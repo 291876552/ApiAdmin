053d8e6442acce6c8c857e30a04ab2b09d463dd0
zhaoxiang <756958008@qq.com>
2017-04-27 10:30:50 +0800

    modified 升级秘钥规则

--- Application/Home/Api/User.class.php
+++ Application/Home/Api/User.class.php
@@ -11,14 +11,16 @@ namespace Home\Api;
 use Home\ORG\Str;
 
 class User extends Base {
-    public function index($param) {
-        $openId = $this->getOpenId($param);
+    public function index($param, $openId = '') {
+        if (!$openId) {
+            $openId = $this->getOpenId($param);
+        }
         $old = D('ApiUser')->where(array('openId' => $openId))->find();
         if ($old) {
             return array('username' => $old['username'], 'password' => '******');
         }
-        $data['username'] = Str::randString(6, 1);
-        $pwd = Str::randString(6, 1);
+        $data['username'] = Str::randString(1, 2) . Str::randString(6, 1);
+        $pwd = Str::randString(1, 2) . Str::randString(6, 1);
         $data['password'] = user_md5($pwd);
         $data['regTime'] = time();
         $data['openId'] = $openId;
@@ -30,16 +32,20 @@ class User extends Base {
 
     public function re($param) {
         $openId = $this->getOpenId($param);
-        $pwd = Str::randString(6, 1);
+        $pwd = Str::randString(1, 2) . Str::randString(6, 1);
         $data['password'] = user_md5($pwd);
         $old = D('ApiUser')->where(array('openId' => $openId))->find();
-        D('ApiUser')->where(array('openId' => $openId))->save($data);
+        if ($old) {
+            D('ApiUser')->where(array('openId' => $openId))->save($data);
 
-        return array('username' => $old['username'], 'password' => $pwd);
+            return array('username' => $old['username'], 'password' => $pwd);
+        } else {
+            return $this->index($param, $openId);
+        }
     }
 
     private function getOpenId($param) {
-        $url = "https://api.weixin.qq.com/sns/jscode2session?appid=wx436e089d55174f47&secret=f4684158a0efd56ff2332582dc62f3db&js_code={$param['code']}&grant_type=authorization_code";
+        $url = "https://api.weixin.qq.com/sns/jscode2session?appid=wx436e089d55174f47&secret=bee53c0bfe94647b21dd2466cfc803b7&js_code={$param['code']}&grant_type=authorization_code";
         $res = file_get_contents($url);
         $resArr = json_decode($res, true);
 
